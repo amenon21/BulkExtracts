9f65da1782a84dc9ffa2272610fdecb27eebd116
Ambily Menon <amen0007@Synchronoss.com>
2017-10-23 15:18:13 +1100

    Added upload component

---
+++ mean/src/app/upload-component/upload-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-component',
+  templateUrl: './upload-component.component.html',
+  styleUrls: ['./upload-component.component.css']
+})
+
+//Upload csv, read csv and write to DB
+export class UploadComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
